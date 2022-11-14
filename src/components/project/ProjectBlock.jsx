5aae833362f52073b530764096944acf30eb37f1
NIKLAUS_REG\mhlie <regnamint@gmail.com>
2022-11-14 20:11:52 +0800

    add techstack, fix on padding

--- src/components/project/ProjectBlock.jsx
+++ src/components/project/ProjectBlock.jsx
@@ -7,18 +7,21 @@ const ProjectBlock = ({ projects }) => {
         <>
             {
                 projects.map((project) => {
-                    const { projectname, description, github, demo } = project;
+                    const { projectname, description, techstack, github, demo } = project;
                     return (
                         <div className="proj-block">
                             <div className="proj-title">
                                 {projectname}
                             </div>
+                            <div className="proj-techstack">
+                                <span>Built by: </span>{techstack.map(language => language + " ")}
+                            </div>
                             <div className="proj-description">
                                 <i>{description}</i>
                             </div>
                             <div className="proj-link">
-                                <a href={github} target="_blank" rel="noopener noreferrer"><i class='bx bxl-github'></i></a>
-                                <a href={demo} target="_blank" rel="noopener noreferrer"><i class='bx bxs-binoculars'></i></a>
+                                <a href={github} target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i></a>
+                                <a href={demo} target="_blank" rel="noopener noreferrer"><i class="fa-sharp fa-solid fa-arrow-up-right-from-square"></i></a>
                             </div>
                         </div>
                     );
